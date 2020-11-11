4beb020da508936024ac494143bfa3b239a1b62c
missaut1309 <57839645+missaut1309@users.noreply.github.com>
2020-11-11 17:18:42 +0700

    update model

--- PythonWeb - Copy/blog/models.py
+++ PythonWeb - Copy/blog/models.py
@@ -1,25 +1,51 @@
 from django.db import models
 from django.conf import settings
 from django.urls import reverse
+from django.db.models import Q
 from django.contrib.auth.models import User
+import operator
+from home.models import Profile
+
 # Create your models here.
+class CategoryManager(models.Manager):
+    def search(self, query=None):
+        qs = self.get_queryset()
+        if query is not None:
+            or_lookup = Q(name__icontains=query)
+            qs = qs.filter(or_lookup).distinct()
+        return qs
+
 class Category(models.Model):
     name = models.CharField(max_length=200)
+
+    objects = CategoryManager()
 
     def __str__(self):
         return self.name
     def get_absolute_url(self):
         return reverse("blog")
     
+class PostManager(models.Manager):
+    def search(self, query=None):
+        qs = self.get_queryset()
+        if query is not None:
+            or_lookup = (
+                Q(title__icontains=query)|
+                Q(body__icontains=query)|
+                Q(author__username__icontains=query)
+            )
+            qs = qs.filter(or_lookup).distinct()
+        return qs
 
 class Post(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=100)
     body = models.TextField(blank=True, null=True)
     image = models.ImageField(null = True, blank=True, upload_to="images/post/")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    snippet = models.CharField(max_length=255,default="Click to link to read post!!")
     likes = models.ManyToManyField(User, related_name='blog_posts', null=True)
 
     def __str__(self):
@@ -31,7 +57,8 @@ class Post(models.Model):
     def total_likes(self):
         return self.likes.count()
 
-    
+    objects = PostManager()
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments_post")
--- PythonWeb - Copy/home/models.py
+++ PythonWeb - Copy/home/models.py
@@ -1,7 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import Q
+
 
 # Create your models here.
+
+class ProfileManager(models.Manager):
+    def search(self,query=None):
+        qs = self.get_queryset()
+        if query is not None:
+            or_lookup = (
+                Q(user__username__icontains=query)|
+                Q(user__first_name__icontains=query)|
+                Q(user__last_name__icontains=query)
+            )
+            qs = qs.filter(or_lookup).distinct()
+        return qs
 
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
@@ -10,5 +24,7 @@ class Profile(models.Model):
 
     def __str__(self):
         return str(self.user)
+
+    objects = ProfileManager()
 
     
